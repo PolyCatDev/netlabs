1d01d56974a7d646aa8ffc60ef8064273225ba13
PolyCat <contact.polycat@proton.me>
2024-11-15 15:36:55 +0200

    FIX: button should be properly animated when pressed

--- objects/button.gd
+++ objects/button.gd
@@ -2,9 +2,10 @@ extends Node3D
 
 @onready var anim = $AnimationPlayer
 
-func _on_area_3d_area_entered(area: Area3D) -> void:
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
 	anim.play("button_press")
 
 
-func _on_area_3d_area_exited(area: Area3D) -> void:
+func _on_area_3d_body_exited(body: Node3D) -> void:
 	anim.play("button_release")
