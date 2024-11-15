e725ceb3efce654b2a65330f1bfa9ae4c77959e0
PolyCat <contact.polycat@proton.me>
2024-11-15 16:46:42 +0200

    added scene switch option to button

--- objects/button.gd
+++ objects/button.gd
@@ -1,10 +1,19 @@
 extends Node3D
 
 @onready var anim = $AnimationPlayer
+@export var change_scene = false
+@export var scene : PackedScene: set = set_scene
 
+var scene_path: String
+
+func set_scene(scene: PackedScene) -> void:
+	scene_path = scene.resource_path
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	anim.play("button_press")
+	
+	if change_scene:
+		get_tree().change_scene_to_file(scene_path)
 
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
